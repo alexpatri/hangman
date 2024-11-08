bfe0cac37bf97b66cd29222898e49ae9775d7546
Alexsandro <jalexsandro2005@gmail.com>
2024-11-08 13:09:59 -0300

    fix: using contructor on main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "game/hangman.hpp"
 
 int main(void) {
-  hangman::Hangman *game = new hangman::Hangman(".words.txt");
+  hangman::Hangman *game = hangman::new_game(".words.txt");
   game->start();
 
   delete game;
